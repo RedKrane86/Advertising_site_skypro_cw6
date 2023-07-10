8260d6d66f8db230c24e22e53746ae32ddbd5904
RedKrane86 <redkrane86@gmail.com>
2023-07-10 09:17:57 +0500

    add token url add refresh url

--- skymarket/skymarket/urls.py
+++ skymarket/skymarket/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("api/admin/", admin.site.urls),
@@ -14,5 +14,8 @@ urlpatterns = [
 
     path("api/", include("users.urls")),
     path("api/", include("ads.urls")),
+
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
